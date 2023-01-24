ed4500215c6d4fa2be3bffaf86b73c37456f85ee
Abir1502 <abirbanerjee1502@gmail.com>
2023-01-24 23:43:32 +0530

    first commit

---
+++ Prime1.java
@@ -0,0 +1,19 @@
+class Prime1{
+public static void main(String args[]){
+int i,j,c;
+for(i=1; i<100; i++);
+{
+c=0;
+for(j=1; j<=i; j++);
+{
+if(i%j==0)
+{
+c++;
+}
+}
+{
+System.out.println("Prime = "+i);
+}
+}
+}
+} 
